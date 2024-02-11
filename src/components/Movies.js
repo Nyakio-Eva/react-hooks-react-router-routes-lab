09ded3a6832fe0e2741f8446c80f304015c4b82c
Nyakio-Eva <evalkariuki@gmail.com>
2024-02-11 10:37:47 +0300

    completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <> 
+     <h1>Movies Page</h1>
+     
+      {movies.map((movie) =>(
+        <div key={movie.title}>
+          {movie.title} {movie.time}
+          <ul>
+            {movie.genres.map((genre)=>(
+              <li key={genre}>
+                 {genre}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+     
+  </>
+  );
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles ={
+  display: "inline-block",
+  width: "100px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "green",
+  textDecoration: "none",
+  color: "white",
+}
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+   <div className="navbar">
+     <NavLink to="/" exact="true" style={linkStyles} activestyle={{background:"darkblue"}}>Home</NavLink>
+     <NavLink to="/actors" style={linkStyles}>Actors</NavLink>
+     <NavLink to="/directors" style={linkStyles}>Directors</NavLink>
+     <NavLink to="/movies" style={linkStyles}>Movies</NavLink>
+   </div>
+  );
 }
 
 export default NavBar;
