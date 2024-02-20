6a4f55fdcce5c1d61cc33aa0577e11ccceafb587
Nyakio-Eva <evalkariuki@gmail.com>
2024-02-20 21:38:24 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory } from "history";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 
